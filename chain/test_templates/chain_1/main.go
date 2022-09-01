c97df95b92968668851997d430e6a41da9c25084
Hiroki <hiroki11hanada@gmail.com>
2022-09-01 23:39:56 +0900

    feature: docker compose deployment

--- chain/test_templates/chain_1/main.go
+++ chain/test_templates/chain_1/main.go
@@ -4,88 +4,113 @@ import (
 	"context"
 	"log"
 	"net"
+  "os"
+  "fmt"
 
-	"github.com/hanapedia/microservice-topologies/chain/chain_1/connections/mongo"
-	pb "github.com/hanapedia/microservice-topologies/chain/chain_1/pb_chain_1"
-	pbc "github.com/hanapedia/microservice-topologies/chain/chain_1/pb_chain_2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/internal/status"
+
+	"github.com/hanapedia/microservice-topologies/chain/test_templates/chain_1/connections/mongo"
+	pb "github.com/hanapedia/microservice-topologies/chain/test_templates/chain_1/pb_chain_1"
+	pbc "github.com/hanapedia/microservice-topologies/chain/test_templates/chain_1/pb_chain_2"
 )
 
 type chain_1Server struct {
 	pb.UnimplementedChain_1Server
 }
 
+const (
+  ListenPort = "3001"
+  ChainNextAddress = "localhost:3002"
+  DbAddress = "mongodb://localhost:27017"
+  DbName = "chain"
+)
+
 var mongoConnection *mongo.Mongo
 
 var nextChain pbc.Chain_2Client
 
-func (s chain_1Server) GetIds(ctx context.Context, req *pb.Req) (*pb.Res, error) {
-  newId, err := mongoConnection.GetItem(req.Ids[len(req.Ids)-1])
-  if err != nil {
-    log.Fatalf("Failed to retrieve item: %v", err)
-  }
-	returnArr := req.Ids
-  returnArr = append(returnArr, newId)
-	// Implement db read logic
-  next := pbc.Req {
-    Ids: returnArr,
-  }
-
-  nextRes, err := nextChain.GetIds(context.Background(), &next)
-  if err != nil {
-    log.Fatalf("Failed to retrieve ids: %v", err)
-  }
-
-  res := pb.Res {
-    Ids: nextRes.Ids,
-  }
-
-	return &res, nil
-}
-
-func (s *chain_1Server) Check(ctx context.Context, req *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
-	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
-}
-
-func (s *chain_1Server) Watch(req *healthpb.HealthCheckRequest, ws healthpb.Health_WatchServer) error {
-	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
-}
-
-func newServer() chain_1Server {
-  return chain_1Server{}
-}
-
 func main() {
-  lis, err := net.Listen("tcp", "localhost:3000")
+	listenPort := ListenPort
+	if os.Getenv("LISTEN_PORT") != "" {
+		listenPort = os.Getenv("LISTEN_PORT")
+	}
+	chainNextAddress := ChainNextAddress
+	if os.Getenv("CHAIN_NEXT_ADDRESS") != "" {
+		chainNextAddress = os.Getenv("CHAIN_NEXT_ADDRESS")
+	}
+	dbAddress := DbAddress
+	if os.Getenv("DB_ADDRESS") != "" {
+		dbAddress = os.Getenv("DB_ADDRESS")
+	}
+	dbName := DbName
+	if os.Getenv("DB_NAME") != "" {
+		dbName = os.Getenv("DB_NAME")
+	}
+
+  lis, err := net.Listen("tcp", fmt.Sprintf(":%s", listenPort))
   if err != nil {
     log.Fatalf("failed to listen: %v", err)
   }
   var opts []grpc.ServerOption
   grpcServer := grpc.NewServer(opts...)
-  svc := newServer()
+  svc := new(chain_1Server)
   pb.RegisterChain_1Server(grpcServer, svc)
-  healthpb.RegisterHealthServer(grpcServer, &svc)
+  healthpb.RegisterHealthServer(grpcServer, svc)
 
-  mongoConnection, err = mongo.InitMongo("mongodb://localhost:27017", "chain", "chain1")
+  mongoConnection, err = mongo.InitMongo(dbAddress, dbName, "chain1")
   if err != nil {
     log.Fatalf("failed to connect to mongodb: %v", err)
   }
+  log.Printf("Established connection with %v", dbAddress)
   defer mongoConnection.Disconnect()
 
   var optsClient []grpc.DialOption
   optsClient = append(optsClient, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-  conn, err := grpc.Dial("localhost:3002", optsClient...)
+  conn, err := grpc.Dial(chainNextAddress, optsClient...)
   if err != nil {
-    log.Printf("Cannot establish connection with the server: %v", err)
+    log.Fatalf("Cannot establish connection with the server: %v", err)
   }
+  log.Printf("Dialed and established connection with %v", chainNextAddress)
   defer conn.Close()
 
   nextChain = pbc.NewChain_2Client(conn)
 
+  log.Printf("Starting server at port %v", listenPort)
   grpcServer.Serve(lis)
+}
+
+func (s chain_1Server) GetIds(ctx context.Context, req *pb.Req) (*pb.Res, error) {
+  next := new(pbc.Req)
+  next.Ids = req.Ids
+  res := new(pb.Res)
+  res.Ids = req.Ids
+
+  newId, err := mongoConnection.GetItem(req.Ids[len(req.Ids)-1])
+  if err == nil {
+    next.Ids = append(next.Ids, newId)
+  } else {
+    log.Fatalf("Failed to retrieve item: %v", err)
+  }
+
+  nextRes, err := nextChain.GetIds(context.Background(), next)
+  if err == nil {
+    res.Ids = nextRes.Ids
+  } else {
+    log.Fatalf("Failed to retrieve ids: %v", err)
+  }
+
+	return res, err
+}
+
+func (s chain_1Server) Check(ctx context.Context, req *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
+	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
+}
+
+func (s chain_1Server) Watch(req *healthpb.HealthCheckRequest, ws healthpb.Health_WatchServer) error {
+	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
 }
